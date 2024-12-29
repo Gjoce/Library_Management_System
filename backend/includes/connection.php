b6cbe904eea0014f99d7f87f1306c368a913260b
Georgi Dimov <gjorgji.dimov@student.um.si>
2024-12-29 22:45:42 +0100

    ef

--- backend/includes/connection.php
+++ backend/includes/connection.php
@@ -21,7 +21,7 @@ try {
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    echo "Connected successfully!";
+    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
